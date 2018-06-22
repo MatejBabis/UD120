b6a348cc4d72ba090c4189c92b8bbcf7d5a30041
Matej Babis <matej@bab.is>
2018-06-22 23:17:45 +0100

    dataset mini project

---
+++ datasets_questions/explore_enron_data.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+"""
+    Starter code for exploring the Enron dataset (emails + finances);
+    loads up the dataset (pickled dict of dicts).
+
+    The dataset has the form:
+    enron_data["LASTNAME FIRSTNAME MIDDLEINITIAL"] = { features_dict }
+
+    {features_dict} is a dictionary of features associated with that person.
+    You should explore features_dict as part of the mini-project,
+    but here's an example to get you started:
+
+    enron_data["SKILLING JEFFREY K"]["bonus"] = 5600000
+
+"""
+
+import pickle
+
+enron_data = pickle.load(
+    open("../final_project/final_project_dataset.pkl", "r"))
+
+# poi_counter = 0
+# for person in enron_data:
+#     if enron_data[person]["poi"] is True:
+#         poi_counter += 1
+# print poi_counter
+
+# print enron_data["PRENTICE JAMES"]["total_stock_value"]
+# print enron_data["COLWELL WESLEY"]["from_this_person_to_poi"]
+# print enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]
+
+# max_payment = 0
+# richest_man = ""
+# for rich_man in ["SKILLING JEFFREY K", "LAY KENNETH L", "FASTOW ANDREW S"]:
+#     if enron_data[rich_man]["total_payments"] > max_payment:
+#         max_payment = enron_data[rich_man]["total_payments"]
+#         richest_man = rich_man
+# print richest_man, max_payment
+
+valid_salary = 0
+known_email = 0
+for person in enron_data:
+    if enron_data[person]["salary"] != 'NaN':
+        valid_salary += 1
+    if enron_data[person]["email_address"] != 'NaN':
+        known_email += 1
+print "valid salary:", valid_salary, "\nvalid email:", known_email
